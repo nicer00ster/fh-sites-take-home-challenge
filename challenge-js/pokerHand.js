9ce48c44e6c8ea270b0acdb4c2374d33cbe42019
Alex Busch <abusch1013@gmail.com>
2019-02-13 11:51:58 -0500

    Setup our blueprint to work with.

--- challenge-js/pokerHand.js
+++ challenge-js/pokerHand.js
@@ -1,5 +1,23 @@
 class PokerHand {
-  constructor() {}
+  constructor(cards) {
+    this.cards = cards;
+    this.deck = [];
+    this.hand = [];
+    this.ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    this.suits = ['c', 'd', 'h', 's'];
+    this.handRank = [
+      'Royal Flush',
+      'Straight Flush',
+      'Four of a Kind',
+      'Full House',
+      'Flush',
+      'Straight',
+      'Three of a Kind',
+      'Two Pair',
+      'One Pair',
+      'High Card',
+    ];
+  }
 
   getRank() {
     // Implement poker hand ranking
